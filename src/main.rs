4f28194083137a236a33341717e4305595a74b1a
Xi Xiao <tdxiaoxi2@gmail.com>
2025-02-22 10:09:00 +0200

    refactoring

---
+++ src/main.rs
@@ -0,0 +1,15 @@
+/// A "Hello, world!" program.
+///
+/// This is the best implementation of this program to ever exist.
+
+fn main() {
+    print_hello();
+    print_goodbye();
+    println!("Hello, world!");
+}
+
+fn print_hello() {
+    println!("Hello, world!");
+    // test
+}
+
